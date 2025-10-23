a627017e35601152558d5c93d4e2b3234d8bf6c7
Aya <ayaazari04@gmail.com>
2025-10-23 19:07:59 +0000

    Premier commit

--- index.js
+++ index.js
@@ -96,19 +96,19 @@ p.then(result => console.log(result)); // affiche " Opération terminée ! " apr
 
 
 // Exercice 10
-// async function getUsers() { 
-//   try { 
-//     const res = await 
-// fetch("https://jsonplaceholder.typicode.com/users"); 
-//     const data = await res.json(); 
-//     console.log(data); 
-//   } catch (e) { 
-//     console.error("Erreur :", e.message); 
-//   } 
-// } 
+async function getUsers() { 
+  try { 
+    const res = await 
+fetch("https://jsonplaceholder.typicode.com/users"); 
+    const data = await res.json(); 
+    console.log(data); 
+  } catch (e) { 
+    console.error("Erreur :", e.message); 
+  } 
+} 
 
  
-// getUsers(); // il affiche : {
+getUsers(); // il affiche : {
 //     id: 1,
 //     name: 'Leanne Graham',
 //     username: 'Bret',
